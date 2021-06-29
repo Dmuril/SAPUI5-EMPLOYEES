6071f430bc217d5ec862296c5c04f86536b9477b
domedil <dilmure@gmail.com>
2021-06-29 19:20:21 +0000

    Primer commit en Modelos y eventos

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,32 +1,88 @@
 //@ts-nocheck
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator ) {
         "use strict";
 
-        return Controller.extend("logaligroup.Employees.controller.MainView", {
-            onInit: function () {
+        function onInit() {
+            var oJSONModel = new sap.ui.model.json.JSONModel();
+            var oView = this.getView();
+            var i18nBundle = oView.getModel("i18n").getResourceBundle();
 
-            },
+            // var oJSON = {
+            //     employeeId : "12345",
+            //     countrykey : "US",
+            //     listCountry : [
+            //         {
+            //             key : "US",
+            //             text : i18nBundle.getText("countryUS")
+            //         },
+            //         {
+            //             key : "UK",
+            //             text : i18nBundle.getText("countryUK")
+            //         },
+            //         {
+            //             key : "ES",
+            //             text : i18nBundle.getText("countryES")
+            //         }
+            //     ]
+            // };
 
-            onValidate: function () {
-                var inputEmployee = this.byId("inputEmployee");
-                var valueEmployee = inputEmployee.getValue();
-                var isSix = ( valueEmployee.length === 6 );
+            //oJSONModel.setData(oJSON);
+            oJSONModel.loadData("./localService/mockdata/Employees.json", false);
+            // oJSONModel.attachRequestCompleted(function(oEventMovel){
+            //     console.log(JSON.stringify(oJSONModel.getData()));
+            // });
+            oView.setModel(oJSONModel);
+        } 
 
-                //if (valueEmployee.length === 6) {
-                   // inputEmployee.setDescription("OK");
-                   this.byId("labelCoutry").setVisible(isSix);
-                   this.byId("slCountry").setVisible(isSix);
-                //} else {
-                //   this.byId("labelCoutry").setVisible(false);
-                //this.byId("slCountry").setVisible(false);
-                // inputEmployee.setDescription("Not OK");
-                //}
+        function onFilter (){
+            var oJSON = this.getView().getModel().getData();
+
+            var filters = [];
+
+            if ( oJSON.EmployeeId !== "" ){
+                filters.push( new Filter("EmployeeID", FilterOperator.EQ, oJSON.EmployeeId));
             }
-        });
-    });
+
+            if ( oJSON.CoutryKey !== "" ){
+                filters.push( new Filter("Country", FilterOperator.EQ, oJSON.CountryKey));
+            }
+
+            var oList = this.getView().byId("tableEmployee");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(filters)
+
+        }
+
+        function onClearFilter (){
+            var oModel = this.getView().getModel();
+            oModel.setProperty("/EmployeeId", "");
+            oModel.setProperty("/CountryKey", "");
+        }
+
+
+
+        var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
+
+        Main.prototype.onValidate = function () {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+            var isSix = (valueEmployee.length === 6);
+
+            this.byId("labelCoutry").setVisible(isSix);
+            this.byId("slCountry").setVisible(isSix);
+        };
+
+        Main.prototype.onInit = onInit;
+        Main.prototype.onFilter = onFilter;
+        Main.prototype.onClearFilter = onClearFilter;
+        return Main;
+    }
+);
