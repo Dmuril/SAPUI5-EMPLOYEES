4ad8347439d19c59464d70f812bb707b62ff50c7
domedil <dilmure@gmail.com>
2021-06-29 01:55:44 +0000

    First Commit

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,32 @@
+//@ts-nocheck
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.Employees.controller.MainView", {
-			onInit: function () {
+        return Controller.extend("logaligroup.Employees.controller.MainView", {
+            onInit: function () {
 
-			}
-		});
-	});
+            },
+
+            onValidate: function () {
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+                var isSix = ( valueEmployee.length === 6 );
+
+                //if (valueEmployee.length === 6) {
+                   // inputEmployee.setDescription("OK");
+                   this.byId("labelCoutry").setVisible(isSix);
+                   this.byId("slCountry").setVisible(isSix);
+                //} else {
+                //   this.byId("labelCoutry").setVisible(false);
+                //this.byId("slCountry").setVisible(false);
+                // inputEmployee.setDescription("Not OK");
+                //}
+            }
+        });
+    });
