8feb38d480723008b2d339a70aefa839229e080f
domedil <dilmure@gmail.com>
2021-07-19 04:28:02 +0000

    Binding y Formatters

--- Employees/webapp/controller/EmployeeDetails.controller.js
+++ Employees/webapp/controller/EmployeeDetails.controller.js
@@ -1,10 +1,54 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "logaligroup/Employees/model/formatter"
+], function (Controller, formatter) {
 
-    return Controller.extend("logaligroup.Employees.controller.EmployeeDetails", {
-        onInit: function () {
 
+    function onInit() {
+
+    };
+
+    function onCreateIncidence() {
+        var tableIncidence = this.getView().byId("tableIncidence");
+        var newIncidence = sap.ui.xmlfragment("logaligroup.Employees.fragment.NewIncidence", this);
+        var incidenceModel = this.getView().getModel("incidenceModel");
+        var odata = incidenceModel.getData();
+        var index = odata.length;
+        odata.push({ index: index + 1 });
+        incidenceModel.refresh();
+        newIncidence.bindElement("incidenceModel>/" + index);
+        tableIncidence.addContent(newIncidence);
+    };
+
+
+    function onDeleteIncedence(oEvent) {
+
+        var tableIncidence = this.getView().byId("tableIncidence");
+        var rowIncidence = oEvent.getSource().getParent().getParent();
+        var incedenceModel = this.getView().getModel("incidenceModel");
+        var odata = incedenceModel.getData();
+        var contectObjec = rowIncidence.getBindingContext("incidenceModel");
+
+        odata.splice(contectObjec.index - 1, 1);
+        for (var i in odata) {
+            odata[i].index = parseInt(i) + 1
         }
-    });
+
+        incedenceModel.refresh();
+        tableIncidence.removeContent(rowIncidence);
+
+        for (var j in tableIncidence.getContent()) {
+            tableIncidence.getContent()[j].bindElement("incidenceModel>/" + j);
+        }
+    }
+
+    var Main = Controller.extend("logaligroup.Employees.controller.EmployeeDetails", {});
+
+    Main.prototype.onInit = onInit;
+    Main.prototype.onCreateIncidence = onCreateIncidence;
+    Main.prototype.Formatter = formatter;
+    Main.prototype.onDeleteIncedence = onDeleteIncedence;
+
+    return Main;
+
 });
