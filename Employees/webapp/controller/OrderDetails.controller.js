29ab123deb7a455ecefd579fa0eca3cd26a511f9
Dmuril <dilmire@gmail.com>
2021-08-16 23:56:22 +0000

    Controles personalizados

--- Employees/webapp/controller/OrderDetails.controller.js
+++ Employees/webapp/controller/OrderDetails.controller.js
@@ -30,7 +30,12 @@ sap.ui.define([
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("RouteMain", true);
             }
-        }
+        }, 
+
+        onClearSignature : function(){
+             var signature = this.byId("signature");
+             signature.clear();
+         }
 
     });
 });
